3a4dfe16a058b5bfc0562a6871d7480fdfc2176e
Mrunal Patekar <83420916+mrunal-pp@users.noreply.github.com>
2024-03-05 10:28:08 -0800

    generate go sdk (#38)
    Generate go-sdk to get the latest changes to the dynamic_syndication_request.

--- isp/test/api_live2_vod_for_organization_test.go
+++ isp/test/api_live2_vod_for_organization_test.go
@@ -301,6 +301,21 @@ func Test_isp_Live2VODForOrganizationApiService(t *testing.T) {
 
 	})
 
+	t.Run("Test Live2VODForOrganizationApiService PostOrgChannelSyndication", func(t *testing.T) {
+
+		t.Skip("skip test")  // remove to run test
+
+		var org string
+		var channelId string
+
+		resp, httpRes, err := apiClient.Live2VODForOrganizationApi.PostOrgChannelSyndication(context.Background(), org, channelId).Execute()
+
+		require.Nil(t, err)
+		require.NotNil(t, resp)
+		assert.Equal(t, 200, httpRes.StatusCode)
+
+	})
+
 	t.Run("Test Live2VODForOrganizationApiService PostOrgClip", func(t *testing.T) {
 
 		t.Skip("skip test")  // remove to run test
