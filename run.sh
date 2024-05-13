00e331c7edb0f8c00c067f818252a1183516769b
lucaspopp-wbd <lucas.popp@wbd.com>
2024-05-13 13:07:09 -0700

    remove it flag

--- run.sh
+++ run.sh
@@ -20,9 +20,7 @@ cp ./prerequisites/.openapi-generator-ignore ./isp/.openapi-generator-ignore
 cp ./prerequisites/convenience._go ./isp/convenience.go
 cp ./prerequisites/client._go ./isp/client.go
 docker build -t generate-sdk . --no-cache --build-arg OPENAPI_SPEC="${OPENAPI_SPEC}" --build-arg OUT=isp
-docker run --rm -it -v ${SCRIPT_DIR}/isp:/go-sdk/isp generate-sdk
-
-git status
+docker run --rm -v ${SCRIPT_DIR}/isp:/go-sdk/isp generate-sdk
 
 # Logicless templates are dumping extra quotes around enum values, so we've
 # added some padding chars and now need to replace the padding + quotes.
