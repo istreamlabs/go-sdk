f20b977fadcfe2d4876e1e826cea962154be5fe8
mrunal-pp <83420916+mrunal-pp@users.noreply.github.com>
2024-08-21 17:45:49 +0000

    re-generate sdk

--- isp/test/api_deprecated_live2_vod_test.go
+++ isp/test/api_deprecated_live2_vod_test.go
@@ -40,24 +40,6 @@ func Test_isp_DeprecatedLive2VODApiService(t *testing.T) {
 
 	})
 
-	t.Run("Test DeprecatedLive2VODApiService DeprecatedClipGetPresentations", func(t *testing.T) {
-
-		t.Skip("skip test")  // remove to run test
-
-		var customerId string
-		var productId string
-		var programId string
-		var vodId string
-		var clipId string
-
-		resp, httpRes, err := apiClient.DeprecatedLive2VODApi.DeprecatedClipGetPresentations(context.Background(), customerId, productId, programId, vodId, clipId).Execute()
-
-		require.Nil(t, err)
-		require.NotNil(t, resp)
-		assert.Equal(t, 200, httpRes.StatusCode)
-
-	})
-
 	t.Run("Test DeprecatedLive2VODApiService DeprecatedClipGetProgramTime", func(t *testing.T) {
 
 		t.Skip("skip test")  // remove to run test
