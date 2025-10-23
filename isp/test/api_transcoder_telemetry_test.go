1880aba97851d5d10221d87b79b09f5df99c2526
mrunal-pp <83420916+mrunal-pp@users.noreply.github.com>
2025-10-23 16:32:17 +0000

    re-generate sdk

--- isp/test/api_transcoder_telemetry_test.go
+++ isp/test/api_transcoder_telemetry_test.go
@@ -22,6 +22,21 @@ func Test_isp_TranscoderTelemetryApiService(t *testing.T) {
 	configuration := openapiclient.NewConfiguration()
 	apiClient := openapiclient.NewAPIClient(configuration)
 
+	t.Run("Test TranscoderTelemetryApiService ListContentSegmentHistory", func(t *testing.T) {
+
+		t.Skip("skip test")  // remove to run test
+
+		var org string
+		var channelId string
+
+		resp, httpRes, err := apiClient.TranscoderTelemetryApi.ListContentSegmentHistory(context.Background(), org, channelId).Execute()
+
+		require.Nil(t, err)
+		require.NotNil(t, resp)
+		assert.Equal(t, 200, httpRes.StatusCode)
+
+	})
+
 	t.Run("Test TranscoderTelemetryApiService ListRawScteHistory", func(t *testing.T) {
 
 		t.Skip("skip test")  // remove to run test
