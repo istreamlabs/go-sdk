fc92ce7cb3c89bdf2988633e252d79af0bd0f221
jpkalbacher <jpkalbacher@gmail.com>
2025-12-17 14:26:00 -0800

    feat: regen against humav2 with openapi spec3.0 (#135)
    going to cut a pre-release of this (generated against int) so I can pull
    it into live-orchestration and confirm it's working

--- isp-slate/model_slate_without_id.go
+++ isp-slate/model_slate_without_id.go
@@ -18,8 +18,8 @@ var _ MappedNullable = &SlateWithoutID{}
 
 // SlateWithoutID struct for SlateWithoutID
 type SlateWithoutID struct {
-	// An optional URL to a JSON Schema document describing this resource
-	Schema *string `json:"$schema,omitempty" format:"uri" doc:"An optional URL to a JSON Schema document describing this resource"`
+	// A URL to the JSON Schema for this object.
+	Schema *string `json:"$schema,omitempty" format:"uri" doc:"A URL to the JSON Schema for this object."`
 	// A friendly slate description.
 	Description string `json:"description" doc:"A friendly slate description."`
 	// The url where the slate can be accessed. Must be a valid URL
